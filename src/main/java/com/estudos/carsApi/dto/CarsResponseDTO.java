7e66a561afe12a79c0d859ffff82a30701eb35d1
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-11-18 15:33:39 -0300

    feat(dto): adiciona DTO para resposta de carros (CarsResponseDTO)

---
+++ src/main/java/com/estudos/carsApi/dto/CarsResponseDTO.java
@@ -0,0 +1,9 @@
+package com.estudos.carsApi.dto;
+
+import com.estudos.carsApi.cars.Cars;
+
+public record CarsResponseDTO(Long id, String nome, String marca, Integer preco) {
+    public CarsResponseDTO(Cars cars){
+        this(cars.getId(), cars.getNome(), cars.getMarca(), cars.getPreco());
+    }
+}
