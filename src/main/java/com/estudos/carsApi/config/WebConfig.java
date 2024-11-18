4bf7511b63dd517cd9ebb6e692cf38f90aeb761b
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-11-18 15:33:37 -0300

    feat(config): adiciona classe de configuração WebConfig

---
+++ src/main/java/com/estudos/carsApi/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.estudos.carsApi.config;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
