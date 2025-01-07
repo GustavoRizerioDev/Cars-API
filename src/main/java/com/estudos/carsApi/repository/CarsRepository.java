898f2f74edf631857de4a4453282b2f3e2e2e9f7
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-01-07 10:59:12 -0300

    :sparkles: feat: Adicionando metodo de achar todos os carros

--- src/main/java/com/estudos/carsApi/repository/CarsRepository.java
+++ src/main/java/com/estudos/carsApi/repository/CarsRepository.java
@@ -3,10 +3,12 @@ package com.estudos.carsApi.repository;
 import com.estudos.carsApi.domain.Cars;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarsRepository extends JpaRepository<Cars, Long> {
     Optional<Cars> findById(Long carId);
     Cars save(Cars car);
     void deleteById(Long carId);
+    List<Cars> findAll();
 }
