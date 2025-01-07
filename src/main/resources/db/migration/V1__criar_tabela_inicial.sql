73fc35d29fc2da718b27f09df96de42023c377e5
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-01-07 14:32:01 -0300

    :sparkles: feat: Adicionando colunas a tabela de banco de dados
    Co-authored-by: GustavoRizerio <GustavoGrizerioG@gmail.com>

--- src/main/resources/db/migration/V1__criar_tabela_inicial.sql
+++ src/main/resources/db/migration/V1__criar_tabela_inicial.sql
@@ -3,5 +3,11 @@ create table Car(
     nome varchar(200),
     marca varchar(100),
     preco int,
+    ano int,
+    cor varchar(100),
+    modelo varchar(100),
+    quilometragem int,
+    portas int,
+    finalPlaca int,
     primary key(id)
 );
